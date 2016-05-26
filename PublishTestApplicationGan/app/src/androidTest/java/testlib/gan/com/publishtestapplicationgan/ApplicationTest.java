00a676c4566f58b7913a4ecce9e152bb662298af
Pranay V <pranay@bonaven.com>
2016-05-26 15:04:34 +0530

    New publish test

---
+++ PublishTestApplicationGan/app/src/androidTest/java/testlib/gan/com/publishtestapplicationgan/ApplicationTest.java
@@ -0,0 +1,13 @@
+package testlib.gan.com.publishtestapplicationgan;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
